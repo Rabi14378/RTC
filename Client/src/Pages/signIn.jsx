7e2a68742e1bf80be60567054307391f531a7460
Rabi14378 <rabibro14378@gmail.com>
2025-02-10 21:27:55 +0545

    updated css of signin and signup

--- Client/src/Pages/signIn.jsx
+++ Client/src/Pages/signIn.jsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import useInput from "@/Hooks/useInput";
 import { Label } from "@radix-ui/react-label";
+import { FcGoogle } from "react-icons/fc";
 
 export default function SignIn() {
   const {
@@ -35,15 +36,18 @@ export default function SignIn() {
     resetEmail("");
     resetPassword("");
   };
-
+  const googleSignInHandler = () => {
+    console.log("sign in with google");
+  };
   return (
     <form
       onSubmit={submitHandler}
-      className="w-full lg:w-[400px] flex flex-col gap-6">
+      className="w-full max-w-sm mx-auto bg-white shadow-lg rounded-2xl p-8 flex flex-col gap-6">
+      <h2 className="text-2xl font-bold text-center text-gray-800">Sign In</h2>
       <div className="flex flex-col">
         <Label
           htmlFor="email"
-          className="text-gray-700 text-lg lg:text-xl font-semibold mb-2">
+          className="text-gray-700 text-lg font-semibold mb-2">
           Email
         </Label>
         <Input
@@ -53,35 +57,38 @@ export default function SignIn() {
           onChange={emailChangeHandler}
           onBlur={emailBlurHandler}
           value={enteredEmail}
-          className={`w-full px-4 py-3 border ronded-md text-gray-800 focus:outline-none focus:ring-2 ${
-            emailHasError
-              ? "border-red-500 focus:ring-red-500"
-              : "border-gray-300 focus:ring-blue-500"
-          }`}
+          className={`w-full px-4 py-3 border rounded-md text-gray-800 focus:outline-none focus:ring-2 transition-all duration-300 
+            ${
+              emailHasError
+                ? "border-red-500 focus:ring-red-500"
+                : "border-gray-300 focus:ring-blue-500"
+            }`}
         />
         {emailHasError && (
-          <p className="text-red-500 text-sm mt-2">Please enter a valid name</p>
+          <p className="text-red-500 text-sm mt-2">
+            Please enter a valid email
+          </p>
         )}
       </div>
       <div className="flex flex-col">
         <Label
           htmlFor="password"
-          className="text-gray-700 text-lg lg:text-xl font-semibold mb-2">
+          className="text-gray-700 text-lg font-semibold mb-2">
           Password
         </Label>
         <Input
-          type="text"
+          type="password"
           id="password"
           placeholder="Enter your password"
           onChange={passwordChangeHandler}
           onBlur={passwordBlurHandler}
           value={enteredPassword}
-          className={`w-full px-4 py-3 border rounded-md text-gray-800 focus:outline-none focus:ring-2 
-        ${
-          passwordHasError
-            ? "border-red-500 focus:ring-red-500"
-            : "border-gray-300 focus:ring-blue-500"
-        }`}
+          className={`w-full px-4 py-3 border rounded-md text-gray-800 focus:outline-none focus:ring-2 transition-all duration-300 
+            ${
+              passwordHasError
+                ? "border-red-500 focus:ring-red-500"
+                : "border-gray-300 focus:ring-blue-500"
+            }`}
         />
         {passwordHasError && (
           <p className="text-red-500 text-sm mt-2">
@@ -89,9 +96,19 @@ export default function SignIn() {
           </p>
         )}
       </div>
-      <div className="mt-6 flex justify-center">
-        <Button className="w=full py-3 bg-blue-600 text-white font-semibold text-lg rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
-          Submit
+      <div className="mt-6 flex flex-col gap-4">
+        <Button className="w-full py-3 bg-blue-600 text-white font-semibold text-lg rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+          Sign In
+        </Button>
+        <div className="flex items-center gap-2">
+          <div className="flex-1 border-t border-gray-300"></div>
+          <span className="text-gray-500 text-sm">or</span>
+          <div className="flex-1 border-t border-gray-300"></div>
+        </div>
+        <Button
+          onClick={googleSignInHandler}
+          className="w-full py-3 flex items-center justify-center gap-3 border border-gray-300 text-gray-700 font-semibold text-lg rounded-md hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:ring-opacity-50">
+          <FcGoogle size={24} /> Sign in with Google
         </Button>
       </div>
     </form>
--- Client/src/Pages/signUp.jsx
+++ Client/src/Pages/signUp.jsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import useInput from "@/Hooks/useInput";
 import { Input } from "@/components/ui/input";
+import { FcGoogle } from "react-icons/fc";
 
 export default function SignUp() {
   const {
@@ -60,14 +61,19 @@ export default function SignUp() {
     emailReset();
     passwordReset();
   };
+
+  const googleSignInHandler = () => {
+    console.log("sign in with google");
+  };
   return (
     <form
       onSubmit={submitHandler}
-      className="w-full lg:w-[400px] flex flex-col gap-6">
+      className="w-full max-w-sm mx-auto bg-white shadow-lg rounded-2xl p-8 flex flex-col gap-6">
+      <h2 className="text-2xl font-bold text-center text-gray-800">Sign Up</h2>
       <div className="flex flex-col">
         <Label
           htmlFor="firstName"
-          className="text-gray-700 text-lg lg:text-xl font-semibold mb-2">
+          className="text-gray-700 text-lg font-semibold mb-2">
           First Name
         </Label>
         <Input
@@ -77,11 +83,12 @@ export default function SignUp() {
           onChange={firstNameChangeHandler}
           onBlur={firstNameBlurHandler}
           value={enteredFirstName}
-          className={`w-full px-4 py-3 border rounded-md text-gray-800 focus:outline-none focus:ring-2 ${
-            firstNameHasError
-              ? "border-red-500 focus:ring-red-500"
-              : "border-gray-300 focus:ring-blue-500"
-          }`}
+          className={`w-full px-4 py-3 border rounded-md text-gray-800 focus:outline-none focus:ring-2 transition-all duration-300 
+            ${
+              firstNameHasError
+                ? "border-red-500 focus:ring-red-500"
+                : "border-gray-300 focus:ring-blue-500"
+            }`}
         />
         {firstNameHasError && (
           <p className="text-red-500 text-sm mt-2">
@@ -92,7 +99,7 @@ export default function SignUp() {
       <div className="flex flex-col">
         <Label
           htmlFor="lastName"
-          className="text-gray-700 text-lg lg:text-xl font-semibold mb-2">
+          className="text-gray-700 text-lg font-semibold mb-2">
           Last Name
         </Label>
         <Input
@@ -102,11 +109,12 @@ export default function SignUp() {
           onChange={lastNameChangeHandler}
           onBlur={lastNameBlurHandler}
           value={enteredLastName}
-          className={`w-full px-4 py-3 border rounded-md text-gray-800 focus:outline-none focus:ring-2 ${
-            lastNameHasError
-              ? "border-red-500 focus:ring-red-500"
-              : "border-gray-300 focus:ring-blue-500"
-          }`}
+          className={`w-full px-4 py-3 border rounded-md text-gray-800 focus:outline-none focus:ring-2 transition-all duration-300 
+            ${
+              lastNameHasError
+                ? "border-red-500 focus:ring-red-500"
+                : "border-gray-300 focus:ring-blue-500"
+            }`}
         />
         {lastNameHasError && (
           <p className="text-red-500 text-sm mt-2">
@@ -114,11 +122,10 @@ export default function SignUp() {
           </p>
         )}
       </div>
-
       <div className="flex flex-col">
         <Label
           htmlFor="email"
-          className="text-gray-700 text-lg lg:text-xl font-semibold mb-2">
+          className="text-gray-700 text-lg font-semibold mb-2">
           Email
         </Label>
         <Input
@@ -128,11 +135,12 @@ export default function SignUp() {
           onChange={emailChangeHandler}
           onBlur={emailBlurHandler}
           value={enteredEmail}
-          className={`w-full px-4 py-3 border rounded-md text-gray-800 focus:outline-none focus:ring-2 ${
-            emailHasError
-              ? "border-red-500 focus:ring-red-500"
-              : "border-gray-300 focus:ring-blue-500"
-          }`}
+          className={`w-full px-4 py-3 border rounded-md text-gray-800 focus:outline-none focus:ring-2 transition-all duration-300 
+            ${
+              emailHasError
+                ? "border-red-500 focus:ring-red-500"
+                : "border-gray-300 focus:ring-blue-500"
+            }`}
         />
         {emailHasError && (
           <p className="text-red-500 text-sm mt-2">
@@ -140,33 +148,45 @@ export default function SignUp() {
           </p>
         )}
       </div>
-
       <div className="flex flex-col">
         <Label
           htmlFor="password"
-          className="text-gray-700 text-lg lg:text-xl font-semibold mb-2">
+          className="text-gray-700 text-lg font-semibold mb-2">
           Password
         </Label>
         <Input
-          placeholder="Type your password here."
+          type="password"
           id="password"
+          placeholder="Type your password here."
           onChange={passwordChangeHandler}
           onBlur={passwordBlurHandler}
           value={enteredPassword}
-          className={`w-full px-4 py-3 border rounded-md text-gray-800 focus:outline-none focus:ring-2 ${
-            passwordHasError
-              ? "border-red-500 focus:ring-red-500"
-              : "border-gray-300 focus:ring-blue-500"
-          }`}
+          className={`w-full px-4 py-3 border rounded-md text-gray-800 focus:outline-none focus:ring-2 transition-all duration-300 
+            ${
+              passwordHasError
+                ? "border-red-500 focus:ring-red-500"
+                : "border-gray-300 focus:ring-blue-500"
+            }`}
         />
         {passwordHasError && (
-          <p className="text-red-500 text-sm mt-2">Please enter a message.</p>
+          <p className="text-red-500 text-sm mt-2">
+            Please enter a valid password.
+          </p>
         )}
       </div>
-
-      <div className="mt-6 flex justify-center">
+      <div className="mt-6 flex flex-col gap-4">
         <Button className="w-full py-3 bg-blue-600 text-white font-semibold text-lg rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
-          Submit
+          Sign Up
+        </Button>
+        <div className="flex items-center gap-2">
+          <div className="flex-1 border-t border-gray-300"></div>
+          <span className="text-gray-500 text-sm">or</span>
+          <div className="flex-1 border-t border-gray-300"></div>
+        </div>
+        <Button
+          onClick={googleSignInHandler}
+          className="w-full py-3 flex items-center justify-center gap-3 border border-gray-300 text-gray-700 font-semibold text-lg rounded-md hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:ring-opacity-50">
+          <FcGoogle size={24} /> Sign up with Google
         </Button>
       </div>
     </form>
