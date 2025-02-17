9820387eb0891ca365df6b3931254e4298366ec9
Rabi14378 <rabibro14378@gmail.com>
2025-02-17 21:28:31 +0545

    server server

--- Server/server.js
+++ Server/server.js
@@ -1,14 +1,37 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 dotenv.config();
 const app = express();
 app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", process.env.CLIENT_URL);
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PATCH,DELETE,OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello world<h1>");
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`server running on port ${port}`);
-});
+const PORT = process.env.PORT || 5000;
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`server running on port : ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
